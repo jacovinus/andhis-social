7b32495fa867f77c6721f8b245019914e7892135
jacovinus <jacovinus@gmail.com>
2021-01-28 08:27:57 +0100

    update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-//
+
+/** Andhis Nodejs server */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
-// Cargar rutas
+
 const user_routes = require('./routes/user');
 const publication_routes = require('./routes/publication');
 const message_routes = require('./routes/message');
 const follow_routes = require('./routes/follow');
 const hotlist_routes = require('./routes/hotlist');
 const like_routes = require('./routes/like');
-// Middlewares
+
 app.use(bodyParser.urlencoded({encoded: false}));
 app.use(bodyParser.json());
 
-// cors configurar cabeceras http
-app.use
-// Configurar cabeceras y cors
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -28,12 +28,12 @@ app.use((req, res, next) => {
 });
 
 
-//rutas
+
 app.use('/api', user_routes);
 app.use('/api', publication_routes);
 app.use('/api', message_routes);
 app.use('/api', follow_routes);
 app.use('/api', hotlist_routes);
 app.use('/api', like_routes);
-// exportar
+
 module.exports = app;
